022c74f8585f1e1e0bc5b51a1916fc44c4cae82c
Tuan Hoang <tuanhm@leon-system.com>
2021-06-29 09:28:59 +0700

    Finish skeleton for project

---
+++ lib/domains/asset_repository/src/asset_model.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'asset_model.freezed.dart';
+
+@freezed
+class AssetModel with _$AssetModel {
+  const factory AssetModel(
+      {String? tag,
+      String? name,
+      String? type,
+      String? make,
+      String? serial,
+      DateTime? created,
+      DateTime? lastupdated,
+      String? condition,
+      String? manageBy,
+      int? timesUsed,
+      bool? isAssigned}) = _AssetModel;
+}
---
+++ lib/domains/asset_repository/src/asset_model.freezed.dart
@@ -0,0 +1,391 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'asset_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$AssetModelTearOff {
+  const _$AssetModelTearOff();
+
+  _AssetModel call(
+      {String? tag,
+      String? name,
+      String? type,
+      String? make,
+      String? serial,
+      DateTime? created,
+      DateTime? lastupdated,
+      String? condition,
+      String? manageBy,
+      int? timesUsed,
+      bool? isAssigned}) {
+    return _AssetModel(
+      tag: tag,
+      name: name,
+      type: type,
+      make: make,
+      serial: serial,
+      created: created,
+      lastupdated: lastupdated,
+      condition: condition,
+      manageBy: manageBy,
+      timesUsed: timesUsed,
+      isAssigned: isAssigned,
+    );
+  }
+}
+
+/// @nodoc
+const $AssetModel = _$AssetModelTearOff();
+
+/// @nodoc
+mixin _$AssetModel {
+  String? get tag => throw _privateConstructorUsedError;
+  String? get name => throw _privateConstructorUsedError;
+  String? get type => throw _privateConstructorUsedError;
+  String? get make => throw _privateConstructorUsedError;
+  String? get serial => throw _privateConstructorUsedError;
+  DateTime? get created => throw _privateConstructorUsedError;
+  DateTime? get lastupdated => throw _privateConstructorUsedError;
+  String? get condition => throw _privateConstructorUsedError;
+  String? get manageBy => throw _privateConstructorUsedError;
+  int? get timesUsed => throw _privateConstructorUsedError;
+  bool? get isAssigned => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $AssetModelCopyWith<AssetModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $AssetModelCopyWith<$Res> {
+  factory $AssetModelCopyWith(
+          AssetModel value, $Res Function(AssetModel) then) =
+      _$AssetModelCopyWithImpl<$Res>;
+  $Res call(
+      {String? tag,
+      String? name,
+      String? type,
+      String? make,
+      String? serial,
+      DateTime? created,
+      DateTime? lastupdated,
+      String? condition,
+      String? manageBy,
+      int? timesUsed,
+      bool? isAssigned});
+}
+
+/// @nodoc
+class _$AssetModelCopyWithImpl<$Res> implements $AssetModelCopyWith<$Res> {
+  _$AssetModelCopyWithImpl(this._value, this._then);
+
+  final AssetModel _value;
+  // ignore: unused_field
+  final $Res Function(AssetModel) _then;
+
+  @override
+  $Res call({
+    Object? tag = freezed,
+    Object? name = freezed,
+    Object? type = freezed,
+    Object? make = freezed,
+    Object? serial = freezed,
+    Object? created = freezed,
+    Object? lastupdated = freezed,
+    Object? condition = freezed,
+    Object? manageBy = freezed,
+    Object? timesUsed = freezed,
+    Object? isAssigned = freezed,
+  }) {
+    return _then(_value.copyWith(
+      tag: tag == freezed
+          ? _value.tag
+          : tag // ignore: cast_nullable_to_non_nullable
+              as String?,
+      name: name == freezed
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String?,
+      type: type == freezed
+          ? _value.type
+          : type // ignore: cast_nullable_to_non_nullable
+              as String?,
+      make: make == freezed
+          ? _value.make
+          : make // ignore: cast_nullable_to_non_nullable
+              as String?,
+      serial: serial == freezed
+          ? _value.serial
+          : serial // ignore: cast_nullable_to_non_nullable
+              as String?,
+      created: created == freezed
+          ? _value.created
+          : created // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      lastupdated: lastupdated == freezed
+          ? _value.lastupdated
+          : lastupdated // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      condition: condition == freezed
+          ? _value.condition
+          : condition // ignore: cast_nullable_to_non_nullable
+              as String?,
+      manageBy: manageBy == freezed
+          ? _value.manageBy
+          : manageBy // ignore: cast_nullable_to_non_nullable
+              as String?,
+      timesUsed: timesUsed == freezed
+          ? _value.timesUsed
+          : timesUsed // ignore: cast_nullable_to_non_nullable
+              as int?,
+      isAssigned: isAssigned == freezed
+          ? _value.isAssigned
+          : isAssigned // ignore: cast_nullable_to_non_nullable
+              as bool?,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$AssetModelCopyWith<$Res> implements $AssetModelCopyWith<$Res> {
+  factory _$AssetModelCopyWith(
+          _AssetModel value, $Res Function(_AssetModel) then) =
+      __$AssetModelCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {String? tag,
+      String? name,
+      String? type,
+      String? make,
+      String? serial,
+      DateTime? created,
+      DateTime? lastupdated,
+      String? condition,
+      String? manageBy,
+      int? timesUsed,
+      bool? isAssigned});
+}
+
+/// @nodoc
+class __$AssetModelCopyWithImpl<$Res> extends _$AssetModelCopyWithImpl<$Res>
+    implements _$AssetModelCopyWith<$Res> {
+  __$AssetModelCopyWithImpl(
+      _AssetModel _value, $Res Function(_AssetModel) _then)
+      : super(_value, (v) => _then(v as _AssetModel));
+
+  @override
+  _AssetModel get _value => super._value as _AssetModel;
+
+  @override
+  $Res call({
+    Object? tag = freezed,
+    Object? name = freezed,
+    Object? type = freezed,
+    Object? make = freezed,
+    Object? serial = freezed,
+    Object? created = freezed,
+    Object? lastupdated = freezed,
+    Object? condition = freezed,
+    Object? manageBy = freezed,
+    Object? timesUsed = freezed,
+    Object? isAssigned = freezed,
+  }) {
+    return _then(_AssetModel(
+      tag: tag == freezed
+          ? _value.tag
+          : tag // ignore: cast_nullable_to_non_nullable
+              as String?,
+      name: name == freezed
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String?,
+      type: type == freezed
+          ? _value.type
+          : type // ignore: cast_nullable_to_non_nullable
+              as String?,
+      make: make == freezed
+          ? _value.make
+          : make // ignore: cast_nullable_to_non_nullable
+              as String?,
+      serial: serial == freezed
+          ? _value.serial
+          : serial // ignore: cast_nullable_to_non_nullable
+              as String?,
+      created: created == freezed
+          ? _value.created
+          : created // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      lastupdated: lastupdated == freezed
+          ? _value.lastupdated
+          : lastupdated // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      condition: condition == freezed
+          ? _value.condition
+          : condition // ignore: cast_nullable_to_non_nullable
+              as String?,
+      manageBy: manageBy == freezed
+          ? _value.manageBy
+          : manageBy // ignore: cast_nullable_to_non_nullable
+              as String?,
+      timesUsed: timesUsed == freezed
+          ? _value.timesUsed
+          : timesUsed // ignore: cast_nullable_to_non_nullable
+              as int?,
+      isAssigned: isAssigned == freezed
+          ? _value.isAssigned
+          : isAssigned // ignore: cast_nullable_to_non_nullable
+              as bool?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_AssetModel implements _AssetModel {
+  const _$_AssetModel(
+      {this.tag,
+      this.name,
+      this.type,
+      this.make,
+      this.serial,
+      this.created,
+      this.lastupdated,
+      this.condition,
+      this.manageBy,
+      this.timesUsed,
+      this.isAssigned});
+
+  @override
+  final String? tag;
+  @override
+  final String? name;
+  @override
+  final String? type;
+  @override
+  final String? make;
+  @override
+  final String? serial;
+  @override
+  final DateTime? created;
+  @override
+  final DateTime? lastupdated;
+  @override
+  final String? condition;
+  @override
+  final String? manageBy;
+  @override
+  final int? timesUsed;
+  @override
+  final bool? isAssigned;
+
+  @override
+  String toString() {
+    return 'AssetModel(tag: $tag, name: $name, type: $type, make: $make, serial: $serial, created: $created, lastupdated: $lastupdated, condition: $condition, manageBy: $manageBy, timesUsed: $timesUsed, isAssigned: $isAssigned)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _AssetModel &&
+            (identical(other.tag, tag) ||
+                const DeepCollectionEquality().equals(other.tag, tag)) &&
+            (identical(other.name, name) ||
+                const DeepCollectionEquality().equals(other.name, name)) &&
+            (identical(other.type, type) ||
+                const DeepCollectionEquality().equals(other.type, type)) &&
+            (identical(other.make, make) ||
+                const DeepCollectionEquality().equals(other.make, make)) &&
+            (identical(other.serial, serial) ||
+                const DeepCollectionEquality().equals(other.serial, serial)) &&
+            (identical(other.created, created) ||
+                const DeepCollectionEquality()
+                    .equals(other.created, created)) &&
+            (identical(other.lastupdated, lastupdated) ||
+                const DeepCollectionEquality()
+                    .equals(other.lastupdated, lastupdated)) &&
+            (identical(other.condition, condition) ||
+                const DeepCollectionEquality()
+                    .equals(other.condition, condition)) &&
+            (identical(other.manageBy, manageBy) ||
+                const DeepCollectionEquality()
+                    .equals(other.manageBy, manageBy)) &&
+            (identical(other.timesUsed, timesUsed) ||
+                const DeepCollectionEquality()
+                    .equals(other.timesUsed, timesUsed)) &&
+            (identical(other.isAssigned, isAssigned) ||
+                const DeepCollectionEquality()
+                    .equals(other.isAssigned, isAssigned)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(tag) ^
+      const DeepCollectionEquality().hash(name) ^
+      const DeepCollectionEquality().hash(type) ^
+      const DeepCollectionEquality().hash(make) ^
+      const DeepCollectionEquality().hash(serial) ^
+      const DeepCollectionEquality().hash(created) ^
+      const DeepCollectionEquality().hash(lastupdated) ^
+      const DeepCollectionEquality().hash(condition) ^
+      const DeepCollectionEquality().hash(manageBy) ^
+      const DeepCollectionEquality().hash(timesUsed) ^
+      const DeepCollectionEquality().hash(isAssigned);
+
+  @JsonKey(ignore: true)
+  @override
+  _$AssetModelCopyWith<_AssetModel> get copyWith =>
+      __$AssetModelCopyWithImpl<_AssetModel>(this, _$identity);
+}
+
+abstract class _AssetModel implements AssetModel {
+  const factory _AssetModel(
+      {String? tag,
+      String? name,
+      String? type,
+      String? make,
+      String? serial,
+      DateTime? created,
+      DateTime? lastupdated,
+      String? condition,
+      String? manageBy,
+      int? timesUsed,
+      bool? isAssigned}) = _$_AssetModel;
+
+  @override
+  String? get tag => throw _privateConstructorUsedError;
+  @override
+  String? get name => throw _privateConstructorUsedError;
+  @override
+  String? get type => throw _privateConstructorUsedError;
+  @override
+  String? get make => throw _privateConstructorUsedError;
+  @override
+  String? get serial => throw _privateConstructorUsedError;
+  @override
+  DateTime? get created => throw _privateConstructorUsedError;
+  @override
+  DateTime? get lastupdated => throw _privateConstructorUsedError;
+  @override
+  String? get condition => throw _privateConstructorUsedError;
+  @override
+  String? get manageBy => throw _privateConstructorUsedError;
+  @override
+  int? get timesUsed => throw _privateConstructorUsedError;
+  @override
+  bool? get isAssigned => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$AssetModelCopyWith<_AssetModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
