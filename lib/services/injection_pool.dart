b6dc3ea86891bab33886128d3ce458070dc83b65
Tuan Hoang <tuanhm@leon-system.com>
2021-07-05 11:21:11 +0700

    Embed db helper in injection pool

--- lib/services/injection_pool.dart
+++ lib/services/injection_pool.dart
@@ -1,11 +1,18 @@
 import 'dart:async';
 
 import 'package:flutter_simple_dependency_injection/injector.dart';
+import '../domains/asset_repository/src/models.dart' as asset_models;
+import '../domains/asset_type_repository/src/models.dart' as asset_type_model;
+import '../domains/user_repository/src/models.dart' as user_model;
 
 import 'database_handler.dart';
 
 mixin InjectionPool {
   static final DatabaseHandler _dbHandler = DatabaseHandler();
+  static final asset_models.Models _asModels = asset_models.Models();
+  static final asset_type_model.Models _atModel = asset_type_model.Models();
+  static final user_model.Models _uModel = user_model.Models();
+
   static Injector? injector;
 
   static Future<void> initInjection() async {
@@ -15,5 +22,14 @@ mixin InjectionPool {
 
     injector!
         .map<DatabaseHandler>((Injector i) => _dbHandler, isSingleton: true);
+
+    injector!
+        .map<asset_models.Models>((Injector i) => _asModels, isSingleton: true);
+    injector!.map<asset_type_model.Models>(
+      (Injector i) => _atModel,
+      isSingleton: true,
+    );
+    injector!
+        .map<user_model.Models>((Injector i) => _uModel, isSingleton: true);
   }
 }
